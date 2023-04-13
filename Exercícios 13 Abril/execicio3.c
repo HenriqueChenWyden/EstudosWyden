ee30536516a805bb22ccea44ed8a1dd2110b23af
HenriqueChenWyden <129029894+HenriqueChenWyden@users.noreply.github.com>
2023-04-13 23:56:32 +0000

    added

--- Exercícios 13 Abril/execicio3.c
+++ Exercícios 13 Abril/execicio3.c
@@ -2,5 +2,27 @@
 
 int main(){
 
+float n1, n2, n3;
+
+printf("Digite o primeiro número: ");
+scanf("%f", &n1);
+
+printf("Digite o segundo número: ");
+scanf("%f", &n2);
+
+printf("Digite o último número: ");
+scanf("%f", &n3);
+
+if (n1 > n2 && n1 > n3) {
+    if (n2 > n3){
+    printf ("Número: %.2f, %.2f, %.2f", n1, n2, n3);
+    }
+    if (n2 < n3){
+       printf ("Número: %.2f, %.2f, %.2f", n1, n3, n2);
+    }
+}
+
+return 0;
 
 }
+
