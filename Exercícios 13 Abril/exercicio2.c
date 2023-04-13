deb9b078d7f982b367081bf9c6557f62bad54a17
HenriqueChenWyden <129029894+HenriqueChenWyden@users.noreply.github.com>
2023-04-13 23:08:50 +0000

    exec 2

--- Exercícios 13 Abril/exercicio2.c
+++ Exercícios 13 Abril/exercicio2.c
@@ -2,12 +2,20 @@
 
 int main(){
 
-int n1, n2, maior;
+int n1, n2;
 
 printf("\nEscreva um número: ");
 scanf("%i", &n1);
 
-printf("\nEscreva o segundo número: ")
+printf("\nEscreva o segundo número: ");
 scanf("%i", &n2);
 
+if (n1 > n2){
+    printf("O maior número é: %.i", n1);
+} else {
+    printf("O maior número é o: %.i", n2);
+} 
+
+
+return 0;
 }
