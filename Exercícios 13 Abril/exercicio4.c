267b8a0213fecdc8ac0a1f4255832c729ca86448
HenriqueChenWyden <129029894+HenriqueChenWyden@users.noreply.github.com>
2023-04-14 02:11:03 +0000

    Exercicio 4 corrigido com sucesso.

--- Exercícios 13 Abril/exercicio4.c
+++ Exercícios 13 Abril/exercicio4.c
@@ -27,13 +27,46 @@ printf("Digite o quarto número: ");
 scanf("%f", &n4);
 }
 
-if (n1 > n2 && n1 > n3){
+if (n1 > n2 && n1 > n3 && n1 > n4) {
     if (n2 > n3){
-        printf("Número: %.2f, %.2f, %.2f, %.2f \n", n1, n2, n3, n4");
+        printf ("Número: %.2f, %.2f, %.2f, %.2f", n1, n2, n3, n4);
+    }
+    
+    if (n2 < n3){
+        printf ("Número: %.2f, %.2f, %.2f, %.2f", n1, n3, n2, n4);
     }
     
 }
 
-return 0;
+if (n2 > n1 && n2 > n3 && n2 > n4) {
+    if (n1 > n3){
+        printf ("Número: %.2f, %.2f, %.2f, %.2f", n2, n1, n3, n4);
+    }
+    
+    if (n1 < n3){
+        printf ("Número: %.2f, %.2f, %.2f, %.2f", n2, n3, n1, n4);
+    }
+    
+}
 
+if (n3 > n1 && n3 > n2 && n3 > n4) {
+    if (n1 > n2){
+        printf ("Número: %.2f, %.2f, %.2f, %.2f", n3, n1, n2, n4);
+    }
+    
+    if (n1 < n2){
+        printf ("Número: %.2f, %.2f, %.2f, %.2f", n3, n2, n1, n4);
+    }
+}
+
+if (n4 > n1 && n4 > n2 && n4 > n3) {
+    if (n3 > n4){
+        printf("Número: %.2f, %.2f, %.2f, %.2f", n4, n3, n1, n2);
+    }
+     if (n3 < n4){
+        printf("Número: %.2f, %.2f, %.2f, %.2f", n4, n3, n2, n1);
+    }
+}
+
+    return 0;
 }
