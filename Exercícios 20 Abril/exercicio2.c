7afb57a99f50ced8de4cadd8fa5fc3cb0e53e1b4
HenriqueChenWyden <129029894+HenriqueChenWyden@users.noreply.github.com>
2023-04-20 03:08:21 +0000

    em realização.

--- Exercícios 20 Abril/exercicio2.c
+++ Exercícios 20 Abril/exercicio2.c
@@ -2,6 +2,23 @@
 
 int main(){
 
-    
+    printf("Calculadora de Salário + Cargo v1\n");
+
+    int codigo, percentual, cargos;
+    float aumentom, salarioatual, salarioaum;
+
+    printf("\nComece por digitar o teu código de cargo, considerando a lista abaixo.");
+
+    printf("\n1 - Escriturário\n 2 - Secretário\n 3 - Caixa\n 4 - Gerente\n 5 - Diretor");
+
+    printf("\nDigite o seu código: ");
+    scanf("%i", &codigo);
+
+    printf("\nSeu código: %i", codigo);
+
+    printf("\n\nDigite agora o seu salário atual: R$ ");
+    scanf("%f", salarioatual);
+
+
 
 }
